4279f933fa8a02dc7d62f35a23fd03db34f3e61c
Augusto Lopes <augustolfp@gmail.com>
2022-09-26 17:19:55 -0300

    test(e2e): Implement POST recommendation tests for invelid cases

--- front-end/cypress/e2e/insertRecommendation.cy.js
+++ front-end/cypress/e2e/insertRecommendation.cy.js
@@ -33,4 +33,60 @@ describe('Test insert recommendation routine', () => {
         cy.url().should('equal', 'http://localhost:3000/');
         cy.contains(newRecommendation.name).should('be.visible');
     });
+    it('Tests if an error occurs if user inserts invalid link', () => {
+        const invalidRecommendation = {
+            name: faker.lorem.words(4),
+            youtubeLink: faker.internet.url()
+        };
+
+        cy.visit('http://localhost:3000');
+        cy.get('[data-cy="nameInput"]').type(invalidRecommendation.name);
+        cy.get('[data-cy="ytLinkInput"]').type(
+            invalidRecommendation.youtubeLink
+        );
+        cy.intercept('POST', 'http://localhost:5000/recommendations').as(
+            'recommInsertion'
+        );
+        cy.get('[data-cy="postButton"]').click();
+        cy.wait('@recommInsertion');
+
+        cy.url().should('equal', 'http://localhost:3000/');
+        cy.contains(invalidRecommendation.name).should('not.exist');
+    });
+    it('Tests if an error occurs if user inserts a name that already exists', () => {
+        const videos = [
+            'https://www.youtube.com/watch?v=UivZrL2znh0&list=RDMM54fea7wuV6s&index=2',
+            'https://www.youtube.com/watch?v=Gi-GlK26Nqk&list=RDMM54fea7wuV6s&index=3',
+            'https://www.youtube.com/watch?v=NPl2N9eQOn4&list=RDMM54fea7wuV6s&index=6'
+        ];
+
+        function pickVideoFromList() {
+            return videos[Math.floor(Math.random() * videos.length)];
+        }
+
+        const newRecommendation = {
+            name: faker.lorem.words(4),
+            youtubeLink: pickVideoFromList()
+        };
+
+        cy.visit('http://localhost:3000');
+        cy.get('[data-cy="nameInput"]').type(newRecommendation.name);
+        cy.get('[data-cy="ytLinkInput"]').type(newRecommendation.youtubeLink);
+        cy.intercept('POST', 'http://localhost:5000/recommendations').as(
+            'recommInsertion'
+        );
+        cy.get('[data-cy="postButton"]').click();
+        cy.wait('@recommInsertion');
+        cy.get('[data-cy="nameInput"]').type(newRecommendation.name);
+        cy.get('[data-cy="ytLinkInput"]').type(newRecommendation.youtubeLink);
+        cy.intercept('POST', 'http://localhost:5000/recommendations').as(
+            'duplicatedInsertion'
+        );
+        cy.get('[data-cy="postButton"]').click();
+        cy.wait('@duplicatedInsertion');
+
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Error creating recommendation!');
+        });
+    });
 });
